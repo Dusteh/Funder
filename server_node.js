c68c3527caf4f0e5d23fde9045cd8a2b48d2edcd
Dustin Hittenmiller <dhittenmiller@live.com>
2013-02-12 07:55:06 -0500

    Update

--- server_node.js
+++ server_node.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
 var app = express();
 
 
+app.use(express.static(__dirname+'/pages'));
 
-app.get("/",function(req,res){
-        
-});
+//app.get("/",function(req,res){
+//        
+//});
+
+app.listen(process.env.PORT,process.env.IP);
