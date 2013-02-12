5dda619003a8982f3e6345c4db84efb3f2fdcd5f
Dustin Hittenmiller <dhittenmiller@live.com>
2013-02-12 00:43:21 -0500

    Commit

--- src/server_node.js
+++
@@ -1,6 +0,0 @@
-var http = require("http");
-http.createServer(function(req,res){
-	res.writeHead(200,{'Content-Type':'text/html'});
-	res.end("<html><head/><body><h1>Hello World!</h1></body></html>");
-}).listen(80,'localhost');
-console.log('Server running at http://localhost');
