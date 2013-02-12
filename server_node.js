024a61ce210f3b4e93024d1c5fc598fbd74cb925
Dustin Hittenmiller <dhittenmiller@live.com>
2013-02-12 00:52:06 -0500

    Update

--- server_node.js
+++ server_node.js
@@ -1,6 +1,9 @@
-var http = require("http");
-http.createServer(function(req,res){
-	res.writeHead(200,{'Content-Type':'text/html'});
-	res.end("<html><head/><body><h1>Hello World!</h1></body></html>");
-}).listen(80,'localhost');
-console.log('Server running at http://localhost');
+var express = require('express');
+
+var app = express();
+
+
+
+app.get("/",function(req,res){
+        
+});
