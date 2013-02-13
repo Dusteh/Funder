bba28191badd0abbd77ba1dd8fc6e52a1b8fb099
Dustin Hittenmiller <dhittenmiller@live.com>
2013-02-12 20:47:52 -0500

    Built main index.html, added other third party components to make it all work

--- server_node.js
+++ server_node.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var app = express();
 
+app.use(express.logger('dev'));
 
-app.use(express.static(__dirname+'/pages'));
+app.use(express.static(__dirname+'/client_components'));
+app.use(express.static(__dirname+'/client_components/third_party_assets'));
+app.use(express.static(__dirname+"/client_components/assets"));
 
-//app.get("/",function(req,res){
-//        
-//});
+app.use(function(req,res,next){
+    console.log('Router handler is:'+ req.url);    
+    next();
+})
+
 
 app.listen(process.env.PORT,process.env.IP);
